1c9fc888ec001b90b1b8c08bafa1980c3e298a17
Daniel Spofford <daniel.spofford@verypossible.com>
2020-03-10 17:47:24 -0500

    Support temperature registers

---
+++ test/nerves_time/rtc/ds3231/temperature_test.exs
@@ -0,0 +1,10 @@
+defmodule NervesTime.RTC.DS3231.TemperatureTest do
+  use ExUnit.Case
+  alias NervesTime.RTC.DS3231.Temperature
+
+  test "decode/1 and encode/1" do
+    data = %{celsius: 25.25}
+    bin = <<0b0001_1001>> <> <<0b0100_0000>>
+    assert {:ok, data} == Temperature.decode(bin)
+  end
+end
