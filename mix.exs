d8fea1d146ea3f7259c85267a8f4036cd4341122
Ryota Kinukawa <kinukawa@tombo-works.com>
2024-06-26 14:17:12 +0900

    v0.1.3 release

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule NervesTime.RTC.DS3231.MixProject do
   use Mix.Project
 
-  @version "0.1.2"
+  @version "0.1.3"
   @source_url "https://github.com/nerves-time/nerves_time_rtc_ds3231"
 
   def project do
